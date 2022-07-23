f50566666d0abf0d55808d4fb9ecd4a56aaa822a
s-yasuoka <migayasu@gmail.com>
2022-07-23 10:59:01 +0900

    fin

--- Main.java
+++ Main.java
@@ -8,20 +8,23 @@ public class Main{
     }
     // 生成した配列の記号を表示
     for (int i = 0; i < seq.length; i++){
-      switch (seq[i]){
-        case 0:
-          System.out.print("A");
-          break;
-        case 1:
-          System.out.print("T");
-          break;
-        case 2:
-          System.out.print("G");
-          break;
-        case 3:
-          System.out.print("C");
-          break;
-      }
+      // switch (seq[i]){
+      //   case 0:
+      //     System.out.print("A");
+      //     break;
+      //   case 1:
+      //     System.out.print("T");
+      //     break;
+      //   case 2:
+      //     System.out.print("G");
+      //     break;
+      //   case 3:
+      //     System.out.print("C");
+      //     break;
+
+      // switch文 書き換え
+      char[] base = {'A', 'T', 'G', 'C'};
+      System.out.println(base[seq[i]] + " ");
     }
   }
 }
